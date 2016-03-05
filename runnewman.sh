4a1c34670d261def1433c11d0632feedae39a7e7
Lazar <lazar@sharenkoto.org>
2016-03-05 14:42:04 +0200

    Lame excuse of bash scripting

--- runnewman.sh
+++ runnewman.sh
@@ -4,6 +4,8 @@ collection=TheBanksAPI.json.postman_collection
 env=TB-VAT.postman_environment
 outfile=test-output.json
 
+cd /home/pi/newt
+
 touch test.lock
-newman --collection $collection --environment $env --outputFile $outfile > out.log
+/usr/local/bin/newman --collection $collection --environment $env --outputFile $outfile > out.log
 rm test.lock
---
+++ runprocess.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/pi/newt
+sudo python ./process-newman-results.py
