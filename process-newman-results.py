ac728922fbce2aec4f388af90877f5a600e52391
Lazar <lazar@sharenkoto.org>
2016-04-03 10:19:06 +0300

    Show IP at start

--- process-newman-results.py
+++ process-newman-results.py
@@ -4,8 +4,16 @@ import json
 import os
 import sys
 import time
+from subprocess import *
 import max7219.led as led
 from max7219.font import proportional, SINCLAIR_FONT, TINY_FONT, CP437_FONT
+
+cmd = "ip -4 addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+
+def run_cmd(cmd):
+        p = Popen(cmd, shell=True, stdout=PIPE)
+        output = p.communicate()[0]
+        return output
 
 # test if .lock file exists and if yes, exit
 if os.path.isfile('test.lock'):
@@ -53,12 +61,16 @@ with open('tick-flag.txt', 'w') as tickFlagFile:
 # draw status icon
 if (runFailed):
     if (tickFlag=='l'):
-        device.letter(0, 1)
+#        device.letter(0, 1)
+        ipaddr = run_cmd(cmd)
+        device.show_message(ipaddr, font=proportional(CP437_FONT))
     else:
-        device.letter(0, 2)
+#        device.letter(0, 2)
+        ipaddr = run_cmd(cmd)
+        device.show_message(ipaddr, font=proportional(CP437_FONT))
 else:
     device.letter(0, 3)
     if (tickFlag=='r'):
         device.scroll_right()
     
-print ('done')
+#print ('done')
